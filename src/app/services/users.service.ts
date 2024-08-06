d94f3d00d146b6d460cbb9ece52b8eb0f26ed255
AdamRse <bakaarion3@gmail.com>
2024-08-06 16:48:16 +0200

    a faire la route post

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -14,6 +14,6 @@ export class UsersService {
     return this.http.get('http://localhost:8000/api/users/nt');
   }
   registerUser(): Observable<any>{
-    return this.http.post("");//
+    return this.http.post("");//https://angular.dev/api/common/http/HttpClient
   }
 }
